0c6f2c79f32abc8eccedef06c2514f10141cbf62
QedimBabayev <49645572+QedimBabayev@users.noreply.github.com>
2020-04-11 15:58:57 +0400

    Add files via upload

---
+++ plugin/js/main.js
@@ -0,0 +1,125 @@
+"use strict"
+
+// $(document).ready(function(){
+
+
+//     //runner width 
+
+// $(".runner").css("width",  $(".menu li.active").outerWidth());
+
+
+// //add active class
+// $(".menu li").click(function(){
+
+//     $(".menu li.active").removeClass("active");
+//     $(this).addClass("active")
+
+
+
+//     let sumOfWidth = 0;
+//     $(this).prevAll().each(function(i, item){
+    
+    
+//     sumOfWidth +=$(item).outerWidth();
+//     });
+    
+//     $(".runner").css({
+//     "left" : sumOfWidth + "px",
+//     "width" : $(this).outerWidth()
+//      })
+
+
+//update runner
+
+
+
+
+
+
+
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     autoplay: true,
+    //     autoplayTimeout:1000,
+    //     margin:10,
+    //     nav:false,
+    //     dots: false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:5
+    //         }
+    //     }
+    // })
+// $("#btn").click(function(){
+
+//  console.log($("#cars").val())
+
+
+// $("input").change(function(){
+
+//   if($("input").is(":checked")){
+
+//     $("#btn").prop("disabled", false)
+//   }
+//   else{
+//     $("#btn").prop("disabled", true)
+
+//   }
+
+// })
+
+
+//4cu tapsiriq
+
+// $(".up").click(function(){
+
+//     $("html, body").animate({ scrollTop: 0 }, 1500);
+//     return false;
+
+// })
+
+
+
+
+
+// $("#btn").click(function(){
+
+//   console.log($("#cars").val())  
+// })
+
+// $("input").change(function(){
+
+//     if($("input").is(":checked")){
+
+//         $("#btn").prop("disabled", false)
+
+
+//     }
+
+//     else{
+//         $("#btn").prop("disabled", true)
+
+//     }
+
+  
+// })
+
+
+// $("#top").click(function(){
+
+
+// $("html, body").animate({scrollTop: 0}, 1000)
+
+// return false
+
+
+// })
+
+
+
